5398f14bb1e3e5dcd6e537207e3c7b9aefb25acf
Nicolai Ruckel <nicolai@ruckel.de>
2019-05-08 13:40:05 +0200

    Add format script

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Bibliography
 
+## Usage
 This repository has to be in `$TEXMFHOME/bibtex/bib/`.
 
 This is my bibliography repository for LaTeX documents. It should be included
@@ -22,3 +23,7 @@ this way:
 
 As soon as others want to compile the document, copy the `.bbl` file, produced
 by BibTex to the directory of the `main.tex` and rename it to `main-lit.tex`.
+
+## Formatting
+The file should always be formatted with the `format.sh` script after adding new
+items.
---
+++ format.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Format the bib file according to DOI rules. Taken from Janek Bevendorff.
+# Requires the `bibtool` and `moreutils` packages.
+
+bibtool -- 'print.align = 24' -- 'print.align.key = 0' \
+        -- 'print.equal.right = off' -- 'pass.comments = on' \
+        -- 'print.line.length = 1000' -- 'print.use.tab = off' \
+        -- 'rewrite.rule = {doi# "https?://.*doi.*\.org/\(10\.[0-9]+/.+\)"# "\1"}' \
+        nruckel.bib | sed '/./,$!d' | sponge nruckel.bib
