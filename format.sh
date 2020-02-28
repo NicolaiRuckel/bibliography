4ac3e1a9e947e03ee4cff19a91db2b57851a0097
Nicolai Ruckel <nicolai@ruckel.de>
2020-02-28 11:34:24 +0100

    Rename bibliography file

--- format.sh
+++ format.sh
@@ -7,4 +7,4 @@ bibtool -- 'print.align = 24' -- 'print.align.key = 0' \
         -- 'print.equal.right = off' -- 'pass.comments = on' \
         -- 'print.line.length = 1000' -- 'print.use.tab = off' \
         -- 'rewrite.rule = {doi# "https?://.*doi.*\.org/\(10\.[0-9]+/.+\)"# "\1"}' \
-        nruckel.bib | sed '/./,$!d' | sponge nruckel.bib
+        literature.bib | sed '/./,$!d' | sponge literature.bib
